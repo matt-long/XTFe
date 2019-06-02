ab063a687d1c9f68b79f7fae776a8acbf0cf5af9
matt-long <mclong@ucar.edu>
2019-06-02 09:37:33 -0600

    year groups added

--- cesm_runs/post-proc.sh
+++ cesm_runs/post-proc.sh
@@ -5,5 +5,6 @@ campaign_path=/gpfs/csfs1/cesm/development/bgcwg/projects/xtFe/cases
 
 CASE=g.e21.G1850ECOIAF.T62_g17.xtfe.001
 ARGS="--components ocn,ice --campaign-transfer --campaign-path ${campaign_path}"
-DEMO=   #"--demo"
+ARGS="${ARGS} --year-groups 1:62,63:124,125:186,187:248,249:310"
+DEMO=  #"--demo"
 ./misc-tools/cesm_hist2tseries.py ${ARGS} ${DEMO} ${CASE}
